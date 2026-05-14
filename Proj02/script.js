5035a41f4448f5a17d6deec5f0182d9c607a8aca
DEV | Samuel Bernardes <dev.samuelbernardes@gmail.com>
2026-05-13 22:04:27 -0300

    Delete Proj02 directory

--- Proj02/script.js
+++
@@ -1,9 +0,0 @@
-const contador = document.querySelector(".contador");
-const clique = document.querySelector(".clique");
-
-let pontos = 0;
-
-clique.addEventListener('click', function(){
-    pontos = pontos + 1;
-    contador.textContent = pontos;
-});
